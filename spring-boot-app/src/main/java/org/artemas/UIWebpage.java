262aabc1b54a126f1f37b8d325d9d3ad9dec4fd9
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2016-09-08 17:58:41 +0100

    ASBA-5 - Made some temp changes to the repo in attempt to include Spring Security.

--- spring-boot-app/src/main/java/org/artemas/UIWebpage.java
+++ spring-boot-app/src/main/java/org/artemas/UIWebpage.java
@@ -7,14 +7,21 @@ import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class UIWebpage {
+public class UIWebpage extends SpringBootServletInitializer {
 	
-	 @RequestMapping("/resource")
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UIWebpage.class);
+    }
+	
+	 @RequestMapping("/user")
 	  public Map<String,Object> home() {
 	    Map<String,Object> model = new HashMap<String,Object>();
 	    model.put("id", UUID.randomUUID().toString());
