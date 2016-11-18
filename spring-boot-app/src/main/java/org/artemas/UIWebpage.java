75a0b8762c98576556a61b02771fa7d2f082bb84
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2016-11-18 11:47:52 +0000

    ASBA-5 - Amended the application to use Social Login via SSO OAuth2.

--- spring-boot-app/src/main/java/org/artemas/UIWebpage.java
+++ spring-boot-app/src/main/java/org/artemas/UIWebpage.java
@@ -1,22 +1,66 @@
 package org.artemas;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 @RestController
-public class UIWebpage extends SpringBootServletInitializer {
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(UIWebpage.class);
-    }
+public class UIWebpage extends WebSecurityConfigurerAdapter{
 	
 	public static void main(String[] args){
 		SpringApplication.run(UIWebpage.class, args);
 	}
+	
+	@Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	      .antMatcher("/**")
+	      .authorizeRequests()
+	        .antMatchers("/", "/login**", "/webjars/**", "/app/**", "/assets/css/**", "/common/bower_components/**","/index.html")
+	        .permitAll()
+	      .anyRequest()
+	        .authenticated()
+	      .and().logout().logoutSuccessUrl("/").permitAll()
+	      .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	  }
+	
+	@RequestMapping("/resource")
+	  public Map<String,Object> home() {
+	    Map<String,Object> model = new HashMap<String,Object>();
+	    model.put("id", UUID.randomUUID().toString());
+	    model.put("content", "Hello World");
+	    return model;
+	  }
+	 
+	
+	/**
+	 * To service the authenticate() function 
+	 * we need to add a new endpoint here in the backend.
+	 * 
+	 * If the “/user” resource is reachable then it will 
+	 * return the currently authenticated user 
+	 * (an Authentication), and otherwise Spring Security 
+	 * will intercept the request and send a 401 response 
+	 * through an AuthenticationEntryPoint.
+	 * 
+	 * @param user
+	 * @return user Principal Obj
+	 */
+	 @RequestMapping("/user")
+	 public Principal user(Principal user){
+		 return user;
+	 }
 
 }
