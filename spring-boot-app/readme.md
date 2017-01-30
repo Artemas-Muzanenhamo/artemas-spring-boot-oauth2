e8262bb5558a6a30d72a05b87ad54bc36d22d6b7
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-01-30 17:04:04 +0000

    Added some code to allow CORS for the domain.

--- spring-boot-app/readme.md
+++ spring-boot-app/readme.md
@@ -5,7 +5,9 @@
 * Run `bower install` to install all the js/css dependencies for the project.
 * To start up the project on your local machine  run `mvn spring-boot:run`
 * To start up the application in `Docker` check `section 2`.
-* To create a docker image of the app you need to run `mvn package docker:build`. *(You can login)*
+* To create a docker image of the app you need to run `mvn package`. *(You can login)*
+* Tags-to-be-pushed can also be specified directly on the command line with
+	`mvn ... docker:build -DpushImageTags -DdockerImageTags=latest -DdockerImageTags=another-tag`
 
 
 ##### Section 2. Running the app on Docker
@@ -15,5 +17,11 @@
 	* Docker
 	* Base Image (e.g. `Centos` or `Ubuntu`)
 
-* Run `docker images` to check if the built image exists.
-* Run the image : `docker run -d -p 8888:8888 --name {appname} {image-name}`
+### Commands - Docker
+- `docker images` : to check if the built image exists.
+- `docker run -d -p {host-port}:{container-port} --name {appname} {image-name}`
+
+### Commands - Maven
+- `mvn release:update-versions` : to update the Snapshot Version in the `pom.xml`
+- `mvn --batch-mode release:update-versions -DdevelopmentVersion=1.2.0-SNAPSHOT` : POM will be set to the version 1.2.0-SNAPSHOT.
+- `mvn clean install -docker-build` : Will build the Snapshot image and put it in the DockerHub Repo.
--- spring-boot-app/src/main/java/org/artemas/UIWebpage.java
+++ spring-boot-app/src/main/java/org/artemas/UIWebpage.java
@@ -8,11 +8,17 @@ import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableOAuth2Sso
@@ -63,4 +69,22 @@ public class UIWebpage extends WebSecurityConfigurerAdapter{
 		 return user;
 	 }
 
+}
+
+@Configuration
+class MyConfiguration {
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://pictopage.com");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 }
